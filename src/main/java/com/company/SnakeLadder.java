d8ad6ccdba49aff8b4f1a6169ccfd91aad92e440
rishikaran0207 <sendittomemrk@gmail.com>
2021-08-26 21:08:11 +0530

    Added: Exactly100

--- src/main/java/com/company/SnakeLadder.java
+++ src/main/java/com/company/SnakeLadder.java
@@ -5,33 +5,47 @@ package com.company;
 
             int Player_Position=0,dicenum;
 
-            for(int i=1; Player_Position <=99;i++)
+            for(int i=1; Player_Position <=99;++i)
             {
-                System.out.println("\nThe Position of the player at starting is " + Player_Position);
+
                 dicenum = (int) (Math.floor(Math.random() * 6 % 7 + 1));
-
-
                 int play = (int) (Math.random() * 3);
-                if (play == 1 || play == 2)
-                    System.out.println("\nThe Number got while rolling dice " + dicenum);
 
 
-                switch (play) {
-                    case 0:
-                        System.out.println("\nThere is no play for player, so the positiion is " + Player_Position);
-                        break;
-                    case 1:
-                        Player_Position = Player_Position + dicenum;
-                        System.out.println("\nThe player position get changed to " + Player_Position);
-                        break;
-                    case 2:
-                        if (Player_Position > dicenum)
-                            Player_Position = Player_Position - dicenum;
-                        else
-                            Player_Position = 0;
-                        System.out.println("\nThe player position get changed to " + Player_Position);
-                        break;
+                if(Player_Position + dicenum >100)
+                {
+                    System.out.println("\nThe Number got while rolling dice " + dicenum+ ", If we add the Position gets above 100 not added");
+                    System.out.println("\nThe Player_Position Reached 100 Successfully!!");
+                    Player_Position=100;
+
                 }
+                else {
+
+
+
+                    switch (play) {
+                        case 0:
+                            System.out.println("\nThere is no play for player, so the positiion is " + Player_Position);
+                            break;
+                        case 1:
+                            if (Player_Position > 100)
+                                Player_Position = 100;
+                            else
+                                System.out.println("\nThe Number got while rolling dice " + dicenum);
+                            Player_Position = Player_Position + dicenum;
+                            System.out.println("\nThe player position get changed to " + Player_Position);
+                            break;
+                        case 2:
+                            if (Player_Position > dicenum)
+                                Player_Position = Player_Position - dicenum;
+                            else
+                                Player_Position = 0;
+                            System.out.println("\nThe Number got while rolling dice " + dicenum);
+                            System.out.println("\nThe player position get changed to " + Player_Position);
+                            break;
+                    }
+                }
+
 
             }
         }
