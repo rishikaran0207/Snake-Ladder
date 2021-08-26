22202b33a98422a77cf5acf6b1dd74b9b357de6c
rishikaran0207 <88846266+rishikaran0207@users.noreply.github.com>
2021-08-26 19:11:06 +0530

    Update SnakeLadder.java

--- src/main/java/com/company/SnakeLadder.java
+++ src/main/java/com/company/SnakeLadder.java
@@ -6,7 +6,5 @@ package com.company;
             int Player_Position = 0;
             System.out.println("\nThe Position of the player at starting is " + Player_Position);
 
-            int dicenum = (int) (Math.floor(Math.random() * 6 % 7 + 1));
-            System.out.println("\nThe Number while rolling dice " + dicenum);
-        }
+                   }
     }
