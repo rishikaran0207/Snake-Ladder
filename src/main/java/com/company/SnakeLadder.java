9b41307bc6f601cd87c485b26d3334e9fb0f0a5d
rishikaran0207 <sendittomemrk@gmail.com>
2021-08-26 22:05:15 +0530

    Added: TwoPlayers

--- src/main/java/com/company/SnakeLadder.java
+++ src/main/java/com/company/SnakeLadder.java
@@ -3,51 +3,95 @@ package com.company;
     public class SnakeLadder {
         public static void main(String[] args) {
 
-            int Player_Position=0,dicenum,count=0;
-            System.out.println("\nThe Position of the player at starting is " + Player_Position+"\n");
-
-            for(int i=1; Player_Position <=99;++i)
-            {
-
-                dicenum = (int) (Math.floor(Math.random() * 6 % 7 + 1));
-                int play = (int) (Math.random() * 3);
+            int Player_Posi1 = 0, Player_Posi2 = 0, dicenum,n=0,count1=0,count2=0;
 
 
-                if(Player_Position + dicenum >100)
-                {
-                    System.out.println("The Number got while rolling dice " + dicenum+ ", If we add the Position gets above 100 not added");
-                    System.out.println("The Player_Position Reached 100 Successfully!!\n");
-                    Player_Position=100;
-
-                }
-                else
-                {
+            for (int j = 0; Player_Posi1 <=99 && Player_Posi2 <=99;j++) {
 
 
+                n=j%2;
 
-                    switch (play)
-                    {
-                        case 0:
-                            System.out.println("There is no play for player, so the positiion is " + Player_Position+"\n");
-                            break;
-                        case 1:
-                            System.out.println("The Number got while rolling dice " + dicenum);
-                            Player_Position = Player_Position + dicenum;
-                            System.out.println("The player position get changed to " + Player_Position+" due to Ladder\n");
-                            break;
-                        case 2:
-                            if (Player_Position > dicenum)
-                                Player_Position = Player_Position - dicenum;
-                            else
-                                Player_Position = 0;
-                            System.out.println("The Number got while rolling dice " + dicenum);
-                            System.out.println("The player position get changed to " + Player_Position+" due to Snake\n");
-                            break;
+
+                switch (n) {
+                    case 0: {
+                        System.out.println("The Player 1\n");
+                        dicenum = (int) (Math.floor(Math.random() * 6 % 7 + 1));
+                        count1++;
+                        int play1 = (int) (Math.random() * 3);
+
+
+                        if (Player_Posi1 + dicenum > 100) {
+                            System.out.println("The Number got while rolling dice " + dicenum + ", If we add the Position gets above 100 not added");
+                            System.out.println("The Player_Position Reached 100 Successfully!!\n");
+                            System.out.println("\n\n    Player 1 Won the game!!!!!!");
+                            Player_Posi1 = 100;
+
+
+                        } else {
+
+
+                            switch (play1) {
+                                case 0:
+                                    System.out.println("There is no play for player, so the positiion is " + Player_Posi1 + "\n");
+                                    break;
+                                case 1:
+                                    System.out.println("The Number got while rolling dice " + dicenum);
+                                    Player_Posi1 = Player_Posi1 + dicenum;
+                                    System.out.println("The player position get changed to " + Player_Posi1 + " due to Ladder\n");
+                                    j++;
+                                    break;
+                                case 2:
+                                    if (Player_Posi1 > dicenum)
+                                        Player_Posi1 = Player_Posi1 - dicenum;
+                                    else
+                                        Player_Posi1 = 0;
+                                    System.out.println("The Number got while rolling dice " + dicenum);
+                                    System.out.println("The player position get changed to " + Player_Posi1 + " due to Snake\n");
+                                    break;
+                            }
+                        }
                     }
-                }
-                count++;
-            }
-            System.out.println("\n The Player roll the dice for "+count+" times to reach Position 100");
-        }
+                    break;
+                    case 1: {
+                        System.out.println("The Player 2\n");
+                        dicenum = (int) (Math.floor(Math.random() * 6 % 7 + 1));
+                        count2++;
+                        int play2 = (int) (Math.random() * 3);
 
+
+                        if (Player_Posi2 + dicenum > 100) {
+                            System.out.println("The Number got while rolling dice " + dicenum + ", If we add the Position gets above 100 not added");
+                            System.out.println("The Player_Position Reached 100 Successfully!!\n");
+                            System.out.println("\n\n    Player 2 Won the game!!!!!!");
+                            Player_Posi2 = 100;
+
+                        } else {
+
+
+                            switch (play2) {
+                                case 0:
+                                    System.out.println("There is no play for player, so the positiion is " + Player_Posi2 + "\n");
+                                    break;
+                                case 1:
+                                    System.out.println("The Number got while rolling dice " + dicenum);
+                                    Player_Posi2 = Player_Posi2 + dicenum;
+                                    System.out.println("The player position get changed to " + Player_Posi2 + " due to Ladder\n");
+                                    j++;
+                                    break;
+
+                                case 2:
+                                    if (Player_Posi2 > dicenum)
+                                        Player_Posi2 = Player_Posi2 - dicenum;
+                                    else
+                                        Player_Posi2 = 0;
+                                    System.out.println("The Number got while rolling dice " + dicenum);
+                                    System.out.println("The player position get changed to " + Player_Posi2 + " due to Snake\n");
+                                    break;
+                            }
+                        }
+                    }
+                    break;
+                }
+            }
+        }
     }
